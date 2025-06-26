30487811f4c0d404468250435fa43ba2d2027e7c
vickyalvandob <73622790+vickyalvandob@users.noreply.github.com>
2025-06-26 13:39:38 +0700

    setup frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
+import ProfilePage from './pages/ProfilePage'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App

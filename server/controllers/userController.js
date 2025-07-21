caa982f19d5b1f6c3d796a6c8d05cb805128c1e2
vickyalvandob <73622790+vickyalvandob@users.noreply.github.com>
2025-07-21 16:58:34 +0700

    add routes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs"
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async () => {
   const {fullName, email, password, bio} = req.body;
@@ -54,4 +55,34 @@ export const login = async () => {
     console.log(error.message)
     return res.json({success:false, message:error.message})
   }
+}
+
+export const checkAuth = (req, res) => {
+  res.json({success: true, user: req.user});
+}
+
+export const updateProfile = async (req, res) => {
+ 
+ try {
+
+  const { profilePic, bio, fullName } = req.body;
+  const userId = req.user._id;
+  let updatedUser;
+
+  if(!profilePic){
+    updatedUser = await User.findByIdAndUpdate(userId, {bio, fullName}, {new:true})
+  } else {
+
+    const upload = await cloudinary.uploader.upload(profilePic);
+
+    updatedUser = await User.findByIdAndUpdate(userId, {profilePic: upload.secure_url, bio, fullName}. {new:true})
+  }
+
+  res.json({success:true, user:updatedUser})
+
+ } catch (error) {
+     console.log(error.message)
+    return res.json({success:false, message:error.message})
+ }
+
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from 'cors';
 import http from 'http';
 import { connectDB } from './lib/db.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 const server = http.createServer(app);
@@ -11,10 +12,13 @@ const server = http.createServer(app);
 app.use(express.json({limit: '50mb'}));
 app.use(cors());
 
+// routes setup
 app.use("api/status", (req, res) => {
   res.send("Server is live")
 });
+app.use("api/auth", userRouter);
 
+// connect db
 await connectDB();
 
 const PORT = process.env.PORT || 5000;

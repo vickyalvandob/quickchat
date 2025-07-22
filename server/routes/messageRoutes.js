9dd9287de1d6ebed137dfc4a6d75b4548e7b479e
vickyalvandob <73622790+vickyalvandob@users.noreply.github.com>
2025-07-22 11:25:12 +0700

    add message controller with socket io

---
+++ server/routes/messageRoutes.js
@@ -0,0 +1,12 @@
+import express from "express"
+import { protectRoute } from "../middleware/auth";
+import { getMessages, getUsersForSidebar, markMessageAsSeen, sendMessage } from "../controllers/messageController";
+
+const messageRouter = express.Router();
+
+messageRouter.get("/users", protectRoute, getUsersForSidebar);
+messageRouter.get("/:id", protectRoute, getMessages);
+messageRouter.put("/mark/:id", protectRoute, markMessageAsSeen);
+messageRouter.post("/send/:id", protectRoute, sendMessage);
+
+export default messageRouter;
--- server/server.js
+++ server/server.js
@@ -4,9 +4,32 @@ import cors from 'cors';
 import http from 'http';
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
+import messageRouter from './routes/messageRoutes.js';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
+
+export const io = new Server(server, {
+  cors: {origin: "*"}
+})
+
+export const userSocketMap = {};
+
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User Connected", userId);
+
+  if(userId) userSocketMap[userId] = socket.id;
+
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
+  })
+})
 
 // middlewares
 app.use(express.json({limit: '50mb'}));
@@ -17,6 +40,7 @@ app.use("api/status", (req, res) => {
   res.send("Server is live")
 });
 app.use("api/auth", userRouter);
+app.use("api/messages", messageRouter)
 
 // connect db
 await connectDB();
